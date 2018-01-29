0bd235cae07ef4d3f6bad4c60f22e709a16be733
Keith Lovett <keithlovett@Keiths-MacBook-Air.local>
2018-01-29 20:28:52 +1300

    updating train

--- javascript/trainAnimation.js
+++ javascript/trainAnimation.js
@@ -50,8 +50,7 @@ var animFrame = {
         clearInterval(this.interval);
         this.canvas.width = 0;
         this.canvas.height = 0;
-        var element = document.getElementById("canvas").outerHTML = "";
-        delete element;
+        canvas.remove();
     }
 }
 
