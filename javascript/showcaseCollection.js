af2a6027ebda31bd69fe08395c3ff3d43308b36c
Keith Lovett <keithlovett@Keiths-MacBook-Air.local>
2018-04-24 23:58:53 +1200

    new frame

--- javascript/showcaseCollection.js
+++ javascript/showcaseCollection.js
@@ -78,12 +78,68 @@ function continueDialogue(e) {
 }
 
 /*
+* ***BEACH STUFF.***
+*/
+
+function startBeachFrame() {
+    if (frame == 2) {
+        stopPremisesFrame();
+    } else if (frame == 1) {
+        stopTrainFrame();
+    }
+    //Creating shapes and image components
+
+    beach = new component(128, 64, "/keiths-site/image_dir/IGBeach.gif", 0, 0, "image", beachFrame);
+
+    if (frame != 3) {
+        BeachFrame.start();
+        frame = 3;
+    } else {
+        stopBeachFrame();
+    }
+}
+
+function stopBeachFrame() {
+    beachFrame.stop();
+    beachFrame.clear();
+}
+
+var beachFrame = {
+    //Creating canvas
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 128;
+        this.canvas.height = 64;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateBeachFrame, 20);
+        text = 0;
+        },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+        this.canvas.width = 0;
+        this.canvas.height = 0;
+    }
+}
+
+function updateBeachFrame() {
+    beachFrame.clear();
+}
+
+
+/*
 * ***TRAIN STUFF.***
 */
 
 function startTrainFrame() {
     if (frame == 2) {
         stopPremisesFrame();
+    } else if (frame == 3) {
+        stopBeachFrame();
     }
     //Creating shapes and image components
 
@@ -222,6 +278,8 @@ function updateTrainFrame() {
     }
 }
 
+
+
 /*
 * ***PREMISES STUFF.***
 */
@@ -229,6 +287,8 @@ function updateTrainFrame() {
 function startPremisesFrame() {
     if (frame == 1) {
         stopTrainFrame();
+    } else if (frame == 3) {
+        stopBeachFrame();
     }
     //Creating shapes and image components
 
@@ -289,6 +349,13 @@ var premisesFrame = {
         this.canvas.height = 0;
     }
 }
+
+
+
+
+
+
+
 
 function updatePremisesFrame() {
 
